ca221c44bd9fe57ea92774feb96627bc825722ee
remuty <remuty.work@gmail.com>
2021-05-27 18:30:12 +0900

    add コンボを追加

--- Assets/Scripts/RhythmManager.cs
+++ Assets/Scripts/RhythmManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RhythmManager : MonoBehaviour
 {
     [SerializeField] private GameObject notePrefab;
     [SerializeField] private Transform[] noteGeneratePositions = new Transform[2];
     [SerializeField] private Transform beatPosition;
+    [SerializeField] private Text comboText;
     
     // [SerializeField] private GameObject[] _notes;
     private List<GameObject> _notes = new List<GameObject>();
@@ -17,10 +19,11 @@ public class RhythmManager : MonoBehaviour
 
     private float _time;
 
-    private float _checkRange = 0.8f;
+    [SerializeField] private float _checkRange = 0.8f;
     
-    private float _beatRange = 0.3f;
-    
+    [SerializeField] private float _beatRange = 0.4f;
+    private int _combo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +46,15 @@ public class RhythmManager : MonoBehaviour
             _time = 0;
             _generateTime = 0.5f;
         }
+
+        if (_combo >= 2)
+        {
+            comboText.text = _combo + "連";
+        }
+        else
+        {
+            comboText.text = "";
+        }
     }
 
     public bool CanBeat()
@@ -53,16 +65,24 @@ public class RhythmManager : MonoBehaviour
             if (Mathf.Abs(_notes[0].transform.position.x) <= _beatRange)
             {
                 Debug.Log("成功:" + "pos:" + _notes[0].transform.position.x);
+                _combo++;
                 ret = true;
             }
             else
             {
                 Debug.Log("ミス:" + "pos:" + _notes[0].transform.position.x);
+                _combo = 0;
             }
             Destroy(_notes[0]);
             Destroy(_notes[1]);
             _notes.RemoveRange(0,2);
         }
+        else
+        {
+            Debug.Log("ミス:" + "pos:" + _notes[0].transform.position.x);
+            _combo = 0;
+        }
+        Debug.Log("コンボ:" + _combo);
         return ret;
     }
 
