9f5b3f1cb659daf8d278ed0e040c9ff7929bf603
remuty <remuty.work@gmail.com>
2021-11-12 06:58:12 +0900

    update RhythmManager

--- Assets/Scripts/Rhythms/RhythmManager.cs
+++ Assets/Scripts/Rhythms/RhythmManager.cs
@@ -10,16 +10,25 @@ public class RhythmManager : MonoBehaviour
     [SerializeField] private Transform beatPosition;
     [SerializeField] private Text comboText;
     [SerializeField] private Text comboSubText;
-    
+    [SerializeField] private double bpm;
+
     // [SerializeField] private GameObject[] _notes;
     private List<GameObject> _notes = new List<GameObject>();
     
     private AudioSource _audio;
+    
+    private double _metronomeStartDspTime;
 
-    private float _generateTime;
-    private float _time = -0.6f;
+    private double _interval;
 
+    private double _buffer;
+
+    private double _time;
+
+    private double _oldTime;
+    
     [SerializeField] private float _checkRange = 0.8f;
+    
     [SerializeField] private float _beatRange = 0.4f;
     private int _combo;
     public int Combo => _combo;
@@ -35,13 +44,16 @@ public class RhythmManager : MonoBehaviour
     {
         _audio = this.GetComponent<AudioSource>();
         _audio.Play();
+        _interval = 1d / (bpm / 60d);
+        _metronomeStartDspTime = AudioSettings.dspTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _time += Time.deltaTime;
-        if (_time > _generateTime)
+        var elapsedDspTime = AudioSettings.dspTime - _metronomeStartDspTime;
+        _time = elapsedDspTime - _oldTime;
+        if (_time > _interval)
         {
             var generatedNote = Instantiate(notePrefab,
                 transform.position, Quaternion.identity, this.transform);
@@ -50,8 +62,16 @@ public class RhythmManager : MonoBehaviour
             generatedNote = Instantiate(notePrefab, transform.position, Quaternion.identity, this.transform);
             generatedNote.GetComponent<Note>().SetTransform(noteGeneratePositions[1],beatPosition);
             _notes.Add(generatedNote);
-            _time = 0;
-            _generateTime = 0.5f;
+            _buffer = _time - _interval;
+            _oldTime += _time - _buffer;
+        }
+
+        if (_notes.Count > 0)
+        {
+            if (Mathf.Abs(_notes[0].transform.position.x) <= 0.001f)
+            {
+                _combo = 0;
+            }
         }
 
         if (_combo >= 2)
@@ -65,6 +85,7 @@ public class RhythmManager : MonoBehaviour
             comboSubText.text = "";
         }
     }
+    
 
     public Beat CanBeat()
     {
@@ -73,11 +94,13 @@ public class RhythmManager : MonoBehaviour
         {
             if (Mathf.Abs(_notes[0].transform.position.x) <= _beatRange)
             {
+                // Debug.Log("成功:" + "pos:" + _notes[0].transform.position.x);
                 _combo++;
                 ret = Beat.good;
             }
             else
             {
+                // Debug.Log("ミス:" + "pos:" + _notes[0].transform.position.x);
                 _combo = 0;
                 ret = Beat.miss;
             }
@@ -98,9 +121,10 @@ public class RhythmManager : MonoBehaviour
             }
         }
     }
-
+    
     public void RemoveNote()
     {
         _notes.RemoveAt(0);
     }
+    
 }
