332f52632837de63b42a5c92d8ab8dfc33d237fb
remuty <remuty.work@gmail.com>
2021-11-16 21:02:21 +0900

    add 得点計算追加

--- Assets/Scripts/Record/Record.cs
+++ Assets/Scripts/Record/Record.cs
@@ -11,7 +11,8 @@ public class Record : MonoBehaviour
     private RecordData _recordData;
     private List<RecordData> _recordDataList;
     private float _seconds, _secondsSum;
-    private int _minutes, _minutesSum, _hours, _hoursSum, _calorie, _calorieSum;
+    private int _minutes, _minutesSum, _hours, _hoursSum, _calorie, _calorieSum, _score;
+
     private bool _isSaved;
     private string _date = DateTime.Today.ToLongDateString();
 
@@ -64,25 +65,29 @@ public class Record : MonoBehaviour
                 _isSaved = true;
                 //カロリー計算 5メッツ*60kg*時間*1.05
                 var time = _hours + _minutes / 60f;
-                _calorie = (int)(5 * 60 * time * 1.05);
+                _calorie = (int) (5 * 60 * time * 1.05);
+                //得点計算 waveごとに500点　10秒ごとに2点
+                _score += 500 * 1;  //TODO:wave数で加算
+                _score += (int) (_minutes * 60 + _seconds) / 10 * 2;    
+                
                 _secondsSum += _seconds;
                 _minutesSum += _minutes;
                 _hoursSum += _hours;
                 _calorieSum += _calorie;
-                _recordData.SetRecord(_secondsSum, _minutesSum, _hoursSum,_calorieSum,_date);
+                _recordData.SetRecord(_secondsSum, _minutesSum, _hoursSum, _calorieSum, _date);
                 _recordDataList.Add(_recordData);
                 _saveLoad.SaveData.SetRecordList(_recordDataList);
                 _saveLoad.Save();
             }
-            //リザルト表示
-            GameObject.Find("Time").GetComponent<Text>().text = $"{_minutes}分{(int)_seconds}秒";
-            GameObject.Find("Calorie").GetComponent<Text>().text = $"{_calorie}kcal";
             
+            //得点、最大波、運動時間、消費カロリー表示
+            GameObject.Find("Score").GetComponent<Text>().text = $"{_score}点";
+            GameObject.Find("Time").GetComponent<Text>().text = $"{_minutes}分{(int) _seconds}秒";
+            GameObject.Find("Calorie").GetComponent<Text>().text = $"{_calorie}kcal";
         }
         else if (_switchScene.Scene == SwitchScene.Scenes.Result1)
         {
-            //リザルト表示
-            
+            //経験値表示
         }
         else if (_switchScene.Scene == SwitchScene.Scenes.Home)
         {
@@ -91,5 +96,10 @@ public class Record : MonoBehaviour
             GameObject.Find("Hours").GetComponent<Text>().text = $"{_hoursSum}";
             GameObject.Find("Calorie").GetComponent<Text>().text = $"{_calorieSum}";
         }
+    }
+
+    public void AddScore(int point)
+    {
+        _score += point;
     }
 }
