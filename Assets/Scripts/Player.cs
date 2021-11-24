4dc2016f4306fea1cc22979bcfd4bd58cb12ffab
remuty <remuty.work@gmail.com>
2021-11-25 01:03:27 +0900

    add しゃがみでバリア

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,8 @@ using UniRx;
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject _resultCanvas;
-
+    [SerializeField] private Image _barrier;
     [SerializeField] private Slider hpGauge;
-
     [SerializeField] private int maxHp;
 
     private int _hp;
@@ -55,12 +54,12 @@ public class Player : MonoBehaviour
             if (n)
             {
                 _isCrouch = true;
-                Debug.Log("しゃがむ");
+                _barrier.enabled = true;
             }
             else
             {
                 _isCrouch = false;
-                Debug.Log("立つ");
+                _barrier.enabled = false;
             }
         });
     }
