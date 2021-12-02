5f741e1951fe14278f5886f3d25c87d78fd6a2ea
remuty <remuty.work@gmail.com>
2021-12-02 20:43:49 +0900

    add Ultがたまった時のSE追加

--- Assets/Scripts/Symbols/UltCard.cs
+++ Assets/Scripts/Symbols/UltCard.cs
@@ -6,26 +6,37 @@ using UnityEngine.UI;
 public class UltCard : MonoBehaviour
 {
     [SerializeField] private Image ultGauge;
-
     [SerializeField]private int _countMax;
+    
+    private SoundManager _sound;
     private float _count;
+    private bool _isMaxed;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _sound = GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_count >= _countMax)
-        {
-            _count = _countMax;
-        }
-        ultGauge.fillAmount = _count / _countMax;
+        
+        
     }
 
     public void AddCount(int combo)
     {
-        _count += combo;
+        if (_count < _countMax)
+        {
+            _count += combo;
+            if (_count >= _countMax)
+            {
+                _count = _countMax;
+                _isMaxed = true;
+                _sound.Ult();
+            }
+            ultGauge.fillAmount = _count / _countMax;
+        }
     }
 }
